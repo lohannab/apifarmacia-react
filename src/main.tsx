ff146047763b29b4c426841dbc18973c4413de60
Lohanna <lohannabt@gmail.com>
2026-05-15 16:08:08 -0300

    fix: corrigir erros de build

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ToastContainer, Bounce } from 'react-toastify'
+import {
+  ToastContainer,
+  Bounce,
+} from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
